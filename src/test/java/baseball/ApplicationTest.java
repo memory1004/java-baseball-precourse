c87309669a199d37e93b9cd1b16fdb7b4a52b444
hue <huttchang@gmail.com>
2022-04-17 00:44:47 +0900

    feat: 테스트케이스변경 테스트케이스의 값에 대한 시나리오를 맞추기 위하여 assertRandomNumberInRangeTest의 인자값 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,14 +11,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+
+    @Test
+    void 서로다른수열테스트() {
+        final List<List<String>> generateNumberList = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            generateNumberList.add(Computer.newInstance().generateNumber());
+        }
+        for (final List<String> numberList : generateNumberList) {
+            for (final String numbers : numberList) {
+                assertThat(!numberList.contains(numbers));
+            }
+        }
+    }
+
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
+                    run("189", "597", "135", "108", "509", "504", "1", "2");
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
-                1, 3, 5, 5, 8, 9
+                108, 3, 108, 5, 8, 9
         );
     }
 
