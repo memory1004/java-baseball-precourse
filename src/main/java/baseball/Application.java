5c6cc08eabedab23a1c9c8fdf5f743b82b3255f8
hue <huttchang@gmail.com>
2022-04-17 00:43:15 +0900

    feat: 게임 진행 함수 호출 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final Game game = Game.getInstance(User.getInstance());
+        game.startGame();
     }
 }
